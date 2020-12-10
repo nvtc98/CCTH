735933ae4fb46dd6b3598c2425e99166a06550f6
Nguyễn Vĩnh Thanh Chương <nvtc.98@gmail.com>
2020-12-10 11:58:09 +0700

    update keyword finding flow

--- public/js/event.js
+++ public/js/event.js
@@ -1,8 +1,11 @@
 $(function() {
     $("#txtData").val(sessionStorage.getItem("data"));
     $("#txtRule").val(sessionStorage.getItem("rule"));
-    $("#txtDataKeyword").val(sessionStorage.getItem("dataKeyword"));
-    $("#txtRuleKeyword").val(sessionStorage.getItem("ruleKeyword"));
+
+    $("#txtDataKeyword1").val(sessionStorage.getItem("dataKeyword1"));
+    $("#txtDataKeyword2").val(sessionStorage.getItem("dataKeyword2"));
+    $("#txtKeywordList").val(sessionStorage.getItem("keywordList"));
+
     $("#txtMatchPhone").val(sessionStorage.getItem("matchPhone"));
     $("#txtMatchKeyword").val(sessionStorage.getItem("matchKeyword"));
 
--- public/js/main.js
+++ public/js/main.js
@@ -29,8 +29,9 @@ const saveSession = () => {
 
 const saveSessionKeyword = () => {
     if (typeof (Storage) !== "undefined") {
-        sessionStorage.setItem('dataKeyword', $("#txtDataKeyword").val());
-        sessionStorage.setItem('ruleKeyword', $("#txtRuleKeyword").val());
+        sessionStorage.setItem('dataKeyword1', $("#txtDataKeyword1").val());
+        sessionStorage.setItem('dataKeyword2', $("#txtDataKeyword2").val());
+        sessionStorage.setItem('keywordList', $("#txtKeywordList").val());
     } else {
         console.log("Session không được hỗ trợ");
     }
@@ -103,54 +104,44 @@ const solve = () => {
     // console.log(ruleTemp, rule,result);
 }
 
-const solveKeyword=()=>{
+const solveKeyword = () => {
     saveSessionKeyword();
 
-    let data = $("#txtDataKeyword").val().split("\n").filter(x => x !== '');
-    let ruleTemp = $("#txtRuleKeyword").val().split("\n\n");
-    let rule = [];
+    let data1 = $("#txtDataKeyword1").val().split("\n").filter(x => x !== '');
+    let data2 = $("#txtDataKeyword2").val().split("\n").filter(x => x !== '');
+    const length = Math.max(data1.length, data2.length);
+    let rule = $("#txtKeywordList").val().split("\n").filter(x => x !== '').map(x => x.split("\t"));
     let result = [];
+    let count = 0, countMain = 0;
 
-    ruleTemp.forEach(x => {
-        let splitter = x.split('\n');
-        if (splitter.length < 2) {
-            return;
-        }
-        let ruleInputs = splitter[0].split(',').map(x => x.trim());
-        let ruleOutputs = splitter.slice(1);
-        rule.push({ ruleInputs, ruleOutputs, randomOrder: 0 });
-    });
-
-    data.forEach(row => {
-        let rowFound = false;
-        for (let i = 0; i < rule.length; ++i) {
-            let matched = false;
-            rule[i].ruleInputs.forEach(x => {
-                // console.log(row.toUpperCase(), "----------", x.toUpperCase(), "----------", row.toUpperCase().search(x.toUpperCase()) === -1);
-                if (!matched && row.toUpperCase().search(x.toUpperCase()) !== -1) {
-                    matched = true;
-                }
-            })
-            if (matched) {
-                // console.log("matched - luật thứ", i + 1, "                      ", row);
-                let order;
-                if (randomOrder) {
-                    order = _.random(rule[i].ruleOutputs.length - 1);
-                }
-                else {
-                    order = rule[i].randomOrder;
-                    rule[i].randomOrder = rule[i].randomOrder < rule[i].ruleOutputs.length - 1 ? rule[i].randomOrder + 1 : 0;
-                }
-                let dataToPush = rule[i].ruleOutputs[order];
-                // console.log("dataToPush", dataToPush, rowIndex);
-                result.push(dataToPush);
-                rowFound = true;
+    for (let i = 0; i < length; ++i) {
+        const row = data1[i];
+        const rowAlt = data2[i];
+        const ruleRow = rule[i] || [];
+        ruleRow.sort().reverse();
+        let keyword = "";
+        for (let j = 0; j < ruleRow.length; ++j) {
+            const ruleUpperCase = ruleRow[j].toUpperCase();
+            const searchMain = row && row.toUpperCase().search(ruleUpperCase) !== -1;
+            if (searchMain || (rowAlt && rowAlt.toUpperCase().search(ruleUpperCase) !== -1)) {
+                keyword = ruleRow[j];
+                searchMain && ++countMain;
+                ++count;
                 break;
             }
         }
-        // console.log(rowFound ? "đã match" : "không có ai match");
-        !rowFound && result.push("");
-    })
+        result.push(keyword);
+    }
+
     $("#txtResultKeyword").val(result.join("\n"));
-    // console.log(ruleTemp, rule,result);
+
+    const rate = _.round(100.0 * count / length, 2), rateMain = _.round(100.0 * countMain / length, 2);
+    const statisticStr = `
+                        Tổng số câu: ${length}<br/>
+                        Số keyword tìm được: ${count}/${length} (${rate}%)<br/>
+                        Số keyword tìm được bởi bản tự dịch: ${countMain}/${length} (${rateMain}%)<br/>
+                        Số keyword tìm được bởi bản dịch shopee: ${count-countMain}/${length} (${_.round(rate - rateMain, 2)}%)<br/>
+                        `;
+    console.log("statisticStr", statisticStr);
+    $("#txtStatisticKeyword").html(statisticStr);
 }
